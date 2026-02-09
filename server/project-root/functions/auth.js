a406589240149a57c24a6fa4fb6760dd78f76134
randyvancofield-tech <randyvancofield@gmail.com>
2026-02-09 12:58:16 -0500

    fix: update auth service and refactor auth.js

--- server/project-root/functions/auth.js
+++ server/project-root/functions/auth.js
@@ -3,10 +3,12 @@ import { createContext } from "../lib/context.js";
 import { getEnv } from "../lib/env.js";
 import { withHandler } from "../lib/handler.js";
 import { authorize } from "../lib/auth.js";
+import { handleAuth } from "../lib/services/index.js";
 
 export const onRequest = withHandler(async (context) => {
   const appContext = await createContext(context);
   getEnv(appContext);
   await authorize(appContext);
-  return json({ success: true });
+  const result = await handleAuth(appContext);
+  return json(result);
 });
---
+++ server/project-root/lib/services/index.js
@@ -0,0 +1,1 @@
+export { handleAuth } from "./authService.js";
