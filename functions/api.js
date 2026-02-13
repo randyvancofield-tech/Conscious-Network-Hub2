768dbee43ff6cd85ffb2b2de95787e7ed3649e0f
randyvancofield-tech <randyvancofield@gmail.com>
2026-02-13 12:19:01 -0500

    fix: update dependencies and fix vulnerabilities

--- functions/api.js
+++
@@ -1,6 +0,0 @@
-import { json } from "../lib/response.js";
-import { withHandler } from "../lib/handler.js";
-
-export const onRequest = withHandler(async () => {
-  return json({ message: "API ready" });
-});
